c7deaec80a9e7837cf227c052f5c853c94a3f556
bwyard <bwyard@gmail.com>
2020-08-04 13:48:17 -0500

    added landing page

--- src/components/landing-page.component.js
+++ src/components/landing-page.component.js
@@ -1,10 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+import VenueList from "./venue-list.component";
 
 export default class LandingPage extends Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    // componentDidMount(){
+    //     axios.get('http://localhost:4000/venues')
+    //         .then(response => {
+    //             this.setState({venues: response.data });
+    //         })
+    //         .catch(function (error){
+    //             console.log(error);
+    //         })
+    // }
+
+
     render(){
         return (
+            
             <div className="container">
-                <p>Welcome to Kid Friendly landing</p>
+                <h1>Kid Friendly Landing page</h1>
+                <Suspense fallback={<div>loading....</div>}>
+                    <VenueList />
+                </Suspense>
             </div>
         )
     }
