d2d21e311fe39270358917d557bbc42f70cfb1a8
bwyard <bwyard@gmail.com>
2020-08-04 07:39:09 -0500

    create navbar and allow linking

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import LandingPage from "./components/landing-page.component";
+import VenueLanding from "./components/venue-landing.component";
+import VenueRating from "./components/venue-rating.component";
+import EditVenue from "./components/edit-venue.component";
+import AddVenue from "./components/add-venue.component";
+import FilterView from "./components/filter-view.component";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">Kid Friendly</Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/">Home</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+                </li>
+                <li className="navbar-item">
+                  <Link to="/add">Add Venue</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+                </li>
+                <li className="navbar-item">
+                  <Link to="/search">Search</Link>
+                </li>
+
+              </ul>
+
+            </div>
+          </nav>
+          <br/>
+
+      
+
+
+      <Route path="/" exact component={LandingPage} />
+      <Route path="/view/:id" component={VenueLanding} />
+      <Route path="/rate/:id" component={VenueRating} />
+      <Router path="/edit/:id" component={EditVenue} />
+      <Router path="/add" component={AddVenue} />
+      <Router path="/search" component={FilterView} />
+      </div>
+    </Router>
   );
 }
 
