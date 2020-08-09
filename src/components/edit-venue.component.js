d824e21d252467c88f6ed35bac67b685e967baa9
bwyard <bwyard@gmail.com>
2020-08-09 17:49:20 -0500

    modifying landing and edit html

--- src/components/edit-venue.component.js
+++ src/components/edit-venue.component.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 export default class EditVenue extends Component{
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeVenueName = this.onChangeVenueName.bind(this);
+        this.onChangeVenueAddress = this.onChangeVenueAddress.bind(this);
+        this.onChangeVenueNumber = this.onChangeVenueNumber.bind(this);
+        this.onChangeVenueRating = this.onChangeVenueRating.bind(this);
+        this.onSubmit=this.onSubmit.bind(this);
+
+        this.state = {
+            venue_name: '',
+            venue_address: '',
+            venue_number: '',
+            venue_rating: ''
+        }
+    }
+
     render(){
         return (
             <div className="container">
                 <p>Welcome to Edit venue Component!</p>
+
             </div>
         )
     }
--- src/components/venue-landing.component.js
+++ src/components/venue-landing.component.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 export default class VenueLanding extends Component{
+
+    constructor(props){
+        super(props);
+
+
+        this.state = {
+            venue_name: '',
+            venue_address: '',
+            venue_number: '',
+            venue_rating: ''
+        }
+    }
+
+
+
     render(){
         return (
             <div className="container">
-                <p>Welcome to Venue Landing!</p>
+                
             </div>
         )
     }
