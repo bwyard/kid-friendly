fc238b7b23c7b4a46357d8c325cfde6de5d38c6c
bwyard <bwyard@gmail.com>
2020-08-12 02:52:06 -0500

    controller created

--- backend-spring/src/main/java/com/kidfriendly/backend/controllers/VenueController.java
+++ backend-spring/src/main/java/com/kidfriendly/backend/controllers/VenueController.java
@@ -1,4 +1,44 @@
 package com.kidfriendly.backend.controllers;
 
+
+import com.kidfriendly.backend.Services.VenueService;
+import com.kidfriendly.backend.models.Venue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class VenueController {
+
+    @Autowired
+    private VenueService venueService;
+
+    @GetMapping("/venue")
+    public List<Venue> get(){
+        return venueService.get();
+    }
+
+    @PostMapping("/venue")
+    public Venue save(@RequestBody Venue venue){
+        venueService.save(venue);
+        return venue;
+    }
+
+    @GetMapping("/venue/{id}")
+    public Venue get(@PathVariable int id){
+        return venueService.get(id);
+    }
+    @DeleteMapping("/employee/{id}")
+    public String delete(@PathVariable int id){
+        venueService.delete(id);
+
+        return "Venue removed with id " + id;
+    }
+    @PutMapping("/venue")
+    public Venue update(@RequestBody Venue venue){
+        venueService.save(venue);
+        return venue;
+    }
 }
